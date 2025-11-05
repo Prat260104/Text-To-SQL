f9b09bd17594a24ebbd75e0ac0abfb86295c91b1
Prateek Rai <prateekrai903@gmail.com>
2025-11-06 00:39:22 +0530

    Delete Desktop/Java-Midsem directory

--- Desktop/Java-Midsem/Question2.java
+++
@@ -1,9 +0,0 @@
-public class Question2 {
-    public static void main(String[] args) {
-        int l=3;
-        int b =4;
-        int h =5;
-        System.out.println(l+b+h);
-        System.out.println(0.5*(b*h));
-    }
-}
